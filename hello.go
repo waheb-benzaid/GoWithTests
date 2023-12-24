34627f4c4ec8e4abec7a7e9a81a674e39639b414
waheb-benzaid <waheb.benzaid@gmail.com>
2023-12-24 16:02:21 +0100

    Add test for multiple languages

--- hello.go
+++ hello.go
@@ -4,14 +4,27 @@ import "fmt"
 
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
+	if name == "" {
+		name= "World"
+	}
+
+	if language == "Spanish" {
+		return spanishHelloPrefix + name
+	}
+
+	if language == "French" {
+		return frenchHelloPrefix + name
+	}
 	return englishHelloPrefix + name
 }
 
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "Spanish"))
 }
 
 
--- hello_test.go
+++ hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T){
 	t.Run("saying hello to people",func(t *testing.T) {
-		got := Hello("Waheb")
+		got := Hello("Waheb","")
 		want := "Hello, Waheb"
 
 		if got != want {
@@ -12,9 +12,21 @@ func TestHello(t *testing.T){
 		}
 	})
 
-	t.Run("say 'Hello, World' When a nempty sting is supplied",func(t *testing.T) {
-		got := Hello("")
+	t.Run("say 'Hello, World' When an empty sting is supplied",func(t *testing.T) {
+		got := Hello("","")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish",func(t *testing.T) {
+		got :=Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French",func(t *testing.T) {
+		got := Hello("Waheb","French")
+		want := "Bonjour, Waheb"
 		assertCorrectMessage(t, got, want)
 	})
 }
