48321f438a3f67b115a0c5f9f5041d78780d42b5
waheb-benzaid <waheb.benzaid@gmail.com>
2023-12-26 18:41:07 +0100

    calculation of the sum of multiple slices

---
+++ arraysAndSlices/sumAll.go
@@ -0,0 +1,12 @@
+package arraysandslices
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
+}
