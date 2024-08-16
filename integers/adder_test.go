3350d1804bdf5f7c42fab9b30fe2fcf16ff12363
waheb-benzaid <waheb.benzaid@gmail.com>
2024-08-16 16:52:05 +0100

    Integer module

---
+++ integers/adder_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Add_test(t *testing.T)  {
+	sum := Add(2,3)
+	expected:= 5
+
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
